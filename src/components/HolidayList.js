9a78d7469c7336dc792f92f05332881f54047f14
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-18 02:59:12 +0530

    Create HolidayList.js

---
+++ src/components/HolidayList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HolidayList = ({ city, country }) => {
+  return (
+    <li>
+      City: {city} , Country: {country}
+    </li>
+  );
+};
+
+export default HolidayList;
